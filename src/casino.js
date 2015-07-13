1bb72d39ae2fd4a47c9b724671250287f8822c7e
Dakota Chambers <dakotachambers@gmail.com>
2015-07-12 22:17:56 -0500

    adding objects to JS file (Game, Player, Staff)

--- src/casino.js
+++ src/casino.js
@@ -1,4 +1,40 @@
-var canvas = document.getElementById('display').getContext('2d');
+//var canvas = document.getElementById('display').getContext('2d');
+
+function update(){
+  //Update tick... run calculations for staff, guests, and each game.
+
+  //draw the new updated screen
+  drawScreen();
+}
+
 function drawScreen(){
+  //update the screen
+}
+
+function Game(gameName, dealer, listOfPlayers, minBet, maxBet){
+  this.gameName = gameName;
+  this.dealer = dealer;
+  this.listOfPlayers = listOfPlayers;
+  this.minBet = minBet;
+  this.maxBet = maxBet;
 
 }
+// var testGame = new Game("Test Game 1", testDealer, <listOfPlayers>, 5, 500);
+
+function Player(playerName, age, gender, money, luckyNumber){
+  this.playerName = playerName;
+  this.age = age;
+  this.gender = gender;
+  this.money = money;
+  this.luckyNumber = luckyNumber;
+}
+//var testPlayer = new Player("Jill", 45, "Female", 1000, 7);
+
+function Staff(staffName, age, gender, job, salary){
+  this.staffName = staffName;
+  this.age = age;
+  this.gender = gender;
+  this.job = job;
+  this.salary = salary;
+}
+// var testDealer = new Staff("Bob", 30, "Male", "Dealer", 15);
