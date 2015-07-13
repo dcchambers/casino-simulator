03e88208e17272e356dde44e72219672656426ae
Dakota Chambers <dakotachambers@gmail.com>
2015-07-13 17:23:01 -0500

    replaced "player" with "guest

--- src/casino.js
+++ src/casino.js
@@ -12,30 +12,30 @@ function drawScreen(){
 }
 
 function test(){
-  var testPlayer = new Player("Jill", 45, "Female", 1000, 7);
+  var testGuest = new Guest("Jill", 45, "Female", 1000, 7);
   var testDealer = new Staff("Bob", 30, "Male", "Dealer", 15);
-  var testGame = new Game("Test Game 1", testDealer, "playerList", 5, 500);
+  var testGame = new Game("Test Game 1", testDealer, "guestList", 5, 500);
 
-  document.getElementById("test").innerText += testDealer.staffName+" | "+testPlayer.playerName+" | "+testGame.gameName;
+  document.getElementById("test").innerText += testDealer.staffName+" | "+testGuest.guestName+" | "+testGame.gameName;
 }
 
-function Game(gameName, dealer, listOfPlayers, minBet, maxBet){
+function Game(gameName, dealer, listOfGuests, minBet, maxBet){
   this.gameName = gameName;
   this.dealer = dealer;
-  this.listOfPlayers = listOfPlayers;
+  this.listOfGusts = listOfGuests;
   this.minBet = minBet;
   this.maxBet = maxBet;
 }
-//var testGame = new Game("Test Game 1", testDealer, <listOfPlayers>, 5, 500);
+//var testGame = new Game("Test Game 1", testDealer, <listOfGuests>, 5, 500);
 
-function Player(playerName, age, gender, money, luckyNumber){
-  this.playerName = playerName;
+function Guest(guestName, age, gender, money, luckyNumber){
+  this.guestName = guestName;
   this.age = age;
   this.gender = gender;
   this.money = money;
   this.luckyNumber = luckyNumber;
 }
-//var testPlayer = new Player("Jill", 45, "Female", 1000, 7);
+//var testGuest = new Guest("Jill", 45, "Female", 1000, 7);
 
 function Staff(staffName, age, gender, job, salary){
   this.staffName = staffName;
@@ -45,3 +45,11 @@ function Staff(staffName, age, gender, job, salary){
   this.salary = salary;
 }
 //var testDealer = new Staff("Bob", 30, "Male", "Dealer", 15);
+
+function createNewStaff(staffName, age, gender, job, salary){
+
+}
+
+function createNewGuest(guestName, age, gender, money, luckyNumber){
+  
+}
